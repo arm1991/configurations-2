76a8e63b091b8507e147b8b6630fe31fe406c8c9
Arman <ghazaryanar@ae.com>
2025-04-01 20:08:41 +0400

    removed comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,27 @@ module.exports = {
   root: true,
   env: { browser: true, es2021: true, node: true },
   extends: [
-    'airbnb', // Airbnb style guide
-    'airbnb-typescript', // Airbnb + TypeScript support
-    'plugin:react/recommended', // React-specific linting rules
-    'plugin:@typescript-eslint/recommended', // TypeScript linting
-    'plugin:prettier/recommended', // Enables Prettier rules
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: { jsx: true },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json', // Required for TypeScript linting
+    project: './tsconfig.json',
   },
   plugins: ['react', '@typescript-eslint', 'react-hooks'],
   rules: {
-    'prettier/prettier': ['error'], // Enforce Prettier formatting
-    'react/react-in-jsx-scope': 'off', // Next.js & React 18 don't need React in scope
-    'react/prop-types': 'off', // TypeScript handles prop validation
-    'import/prefer-default-export': 'off', // Allow named exports
-    'no-console': 'warn', // Warn about console logs
+    'prettier/prettier': ['error'],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'import/prefer-default-export': 'off',
+    'no-console': 'warn',
     'import/extensions': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn'], // Warn for unused variables
     '@typescript-eslint/no-unused-vars': [
       'warn',
       { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
