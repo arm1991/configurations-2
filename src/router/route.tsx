465a1c89178593c650c92e0216887dc9d1f5bee3
Arman <ghazaryanar@ae.com>
2025-04-01 20:23:16 +0400

    remove comments

--- src/router/route.tsx
+++ src/router/route.tsx
@@ -1,28 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-// Pages
 import HomePage from '@/pages/HomePage';
 import AboutPage from '@/pages/AboutPage';
-// Auth
-// import LoginPage from "../pages/LoginPage";
-// import RegisterPage from "../pages/RegisterPage";
-// import ForgotPasswordPage from "../pages/ForgotPasswordPage";
-// import ComingSoonPage from '../pages/ComingSoonPage';
+
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
-      {/* <Route path="/products" element={<ProductsPage />} />
-      <Route path="/view/:id" element={<ViewPage />} />
-      <Route path="/basket" element={<BasketPage />} />
-      <Route path="/contact" element={<ContactPage />} />
-      <Route path="/wish-list" element={<WishListPage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="/menu" element={<MenuPage />}></Route>
-      <Route path="/coming-soon" element={<ComingSoonPage />}></Route> */}
-      {/* <Route path="/sign-in" element={<LoginPage />} />
-      <Route path="/sign-up" element={<RegisterPage />} />
-      <Route path="/forgot-password" element={<ForgotPasswordPage />} /> */}
     </Routes>
   );
 }
